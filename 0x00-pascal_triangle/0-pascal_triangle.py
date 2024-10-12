085fe8fee82abef00365a0d4ba9894e8e61ab674
francis <krypton@gmail.com>
2024-10-12 22:44:46 +0100

    Pascal's Triangle

---
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -0,0 +1,18 @@
+#!/user/bin/python3
+def pascal_triangle(n):
+    """Returns a list of lists representing Pascal's Triangle of size n."""
+    if n <= 0:
+        return []
+
+    triangle = [[1]]  # Initialize the first row of the triangle
+
+    for i in range(1, n):
+        prev_row = triangle[-1]  # Get the previous row
+        new_row = [1]  # Start the new row with 1
+        # Fill in the middle values using the previous row
+        for j in range(1, i):
+            new_row.append(prev_row[j - 1] + prev_row[j])
+        new_row.append(1)  # End the new row with 1
+        triangle.append(new_row)
+
+    return triangle
