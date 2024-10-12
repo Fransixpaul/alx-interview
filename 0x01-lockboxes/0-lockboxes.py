0c3d2b1ba3c99e96562b540d6cbf9ab51d39a916
fransix <kayininse@gmail.com>
2024-10-12 13:32:31 +0100

    Lockboxes

---
+++ 0x01-lockboxes/0-lockboxes.py
@@ -0,0 +1,20 @@
+#!/user/bin/python3
+"""
+This module contains a function to determine if all boxes can be unlocked
+based on the keys found in each box.
+"""
+
+def canUnlockAll(boxes):
+    n= len(boxes)
+    unlocked = set([0])
+    keys = [0]
+
+    while keys:
+        current_key = keys.pop()
+
+        for key in boxes[current_key]:
+            if key not in unlocked and key < n:
+                unlocked.add(key)
+                keys.append(key)
+
+    return len(unlocked) == n
